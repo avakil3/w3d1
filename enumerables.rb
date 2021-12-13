50b989022dda9caeb60cb2319cd0fa5353ac448c
avakil3 <aagamvakil@hotmail.com>
2021-12-13 15:31:34 -0500

    add my_reject method

--- enumerables.rb
+++ enumerables.rb
@@ -20,9 +20,14 @@ class Array
         new_array
     end
 
+    def my_reject(&prc)
+        self.select {|ele| !prc.call(ele) }
+    end
+
 end
 
+
 a = [1, 2, 3]
-p a.my_select { |num| num > 1 } # => [2, 3]
-p a.my_select { |num| num == 4 } # => []
+p a.my_reject { |num| num > 1 } # => [1]
+p a.my_reject { |num| num == 4 } # => [1, 2, 3]
 
