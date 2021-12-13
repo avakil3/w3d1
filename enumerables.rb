765fe2c3e03cde4f94df0ac0fef5b896892d32b5
hwkcode <hwkcode@gmail.com>
2021-12-13 17:00:32 -0500

    Added my_zip version 1

--- enumerables.rb
+++ enumerables.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Array
     def my_each(&prc)
         i = 0
@@ -46,8 +47,28 @@ class Array
         flattened
     end
 
+    def my_zip(*arr)   
+    #     debugger                                                            # [[ 4, 5, 6 ], [ 7, 8, 9 ]]
+    #     new_array = Array.new(self.length) { Array.new(arr.length + 1) }    # [[1, nil, nil], [nil, nil, nil], [nil, nil, nil]]
+    #     self.each_with_index do |ele, idx1|                                 # idx1 => 0
+    #         new_array[idx1][0] = ele                                        # new_array[0][0] = 1
+    #         arr.each_with_index do |subarray, idx2|                         # subarray => [4, 5, 6] idx2 => 0
+    #             subarray.each_with_index do |ele, idx3|                     # ele => 4, idx3 => 0
+    #                 new_array[idx1][idx3] = subarray[idx2] if idx3 != 0     # new_array[0][1] = subarray[0] if idx3 != 0
+    #             end
+    #         end
+    #     end
+    #     print new_array
+        new_array = Array.new(self.length) { [] }                               # [[], [], []]
+
+        
+    end
+
 end
 
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+[1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
 
 
 
