9217ea866540c259dfb12ca5ae81fe8c05254f2e
hwkcode <hwkcode@gmail.com>
2021-12-13 15:27:17 -0500

    Add my_select

--- enumerables.rb
+++ enumerables.rb
@@ -7,6 +7,22 @@ class Array
         end
         self
     end
+
+    def my_select(&prc)
+        new_array = []
+
+        i = 0
+        while i < self.length
+            new_array << self[i] if prc.call(self[i])
+            i += 1
+        end
+
+        new_array
+    end
+
 end
 
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
 
